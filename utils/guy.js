ce1230b92837838ae5ac4f8b60deaa623cedb741
Guy Murray (TAG ZA) <gmurray@tagworldwide.com>
2016-05-16 17:31:40 +0200

    GM: fixored includes md5 thing

--- utils/guy.js
+++ utils/guy.js
@@ -1,10 +1,11 @@
+var md5 = require('md5');
 module.exports = {
-  md5: require('md5'),
+  md5: md5,
   prepUser: function (aUser, aSocket) {
     var user = {
       email: aUser.username,
       username: aUser.username.split('@')[0],
-      password: this.md5(aUser.password),
+      password: md5(aUser.password),
       socketID: aSocket.id
     };
     return user;
