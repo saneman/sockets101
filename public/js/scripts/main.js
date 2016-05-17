aa1dae5664f268ce96e84c318ace4df6df4ab8ce
Guy Murray (TAG ZA) <gmurray@tagworldwide.com>
2016-05-17 08:52:24 +0200

    GM: added case game with collision

--- public/js/scripts/main.js
+++ public/js/scripts/main.js
@@ -13,6 +13,23 @@ $(function() {
     gUser = {},
     $app = $('.app'),
 
+    collision = function ($div1, $div2) {
+      var x1 = $div1.offset().left;
+      var y1 = $div1.offset().top;
+      var h1 = $div1.outerHeight(true);
+      var w1 = $div1.outerWidth(true);
+      var b1 = y1 + h1;
+      var r1 = x1 + w1;
+      var x2 = $div2.offset().left;
+      var y2 = $div2.offset().top;
+      var h2 = $div2.outerHeight(true);
+      var w2 = $div2.outerWidth(true);
+      var b2 = y2 + h2;
+      var r2 = x2 + w2;
+
+      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+      return true;
+    },
 
     displayUsers = function (aUser) {
       var
@@ -178,7 +195,8 @@ $(function() {
             direction = aData.direction,
             top = $button.position().top,
             left = $button.position().left,
-            inc = 15;
+            inc = 15,
+            hit;
 
           switch (direction) {
             case 'forward':
@@ -193,6 +211,13 @@ $(function() {
             case 'right':
               $button.css('left',  '+=' + inc + 'px');
               break;
+          }
+
+          hit = $('.under-their-control').length !== 0 ?
+            collision($('.under-my-control'), $('.under-their-control')) : false;
+
+          if (hit) {
+            console.log('you got him!!!!!');
           }
 
           $button.css('position', 'relative');
